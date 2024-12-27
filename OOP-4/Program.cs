7825d1cc79bd72e8716c425ec5f38ba8951209f4
Mohab Khaled <mohabk044@gmail.com>
2024-12-27 09:02:07 +0200

    Video 5

--- OOP-4/Program.cs
+++ OOP-4/Program.cs
@@ -39,12 +39,40 @@ namespace Demo
             referenceFromInterface.Print();
             #endregion
 
+            #region InterfaceEx02
+            //SeriesByTwo seriesByTwo = new SeriesByTwo();
+            //PrintTenNumbersFromSeries(seriesByTwo);
+            //SeriesByThree seriesByThree = new SeriesByThree();
+            //PrintTenNumbersFromSeries(seriesByThree);
+            //SeriesByFour seriesByFour = new SeriesByFour();
+            //PrintTenNumbersFromSeries(seriesByFour);
+            #endregion
+
             #region InterfaceEx03
             IMovable movable = new Airplane();
             movable.Forward();
             IFlyable flyable = new Airplane();
             flyable.Forward();
             #endregion
+
+            #region Video 5
+
+            int[] Arr01 = [1, 2, 3];
+            int[] Arr02 = [4, 5, 6];
+            Console.WriteLine($"Arr01.GetHashCode() : {Arr01.GetHashCode()}");
+            Console.WriteLine($"Arr02.GetHashCode() : {Arr02.GetHashCode()}");
+            Arr02 = Arr01; // this object [1,2,3] has 2 references [Arr01,Arr02] , [4,5,6] became unreachable object.
+            //Console.WriteLine("After Shallow Copy");
+            Console.WriteLine("After Deep Copy");
+            Arr02 = (int[])Arr01.Clone();
+            Console.WriteLine($"Arr01.GetHashCode() : {Arr01.GetHashCode()}");
+            Console.WriteLine($"Arr02.GetHashCode() : {Arr02.GetHashCode()}");
+            Arr02[0] = 100;
+            foreach (int number in Arr01)
+                Console.WriteLine(number);
+            Console.WriteLine("");
+            #endregion
+
         }
     }
 }
