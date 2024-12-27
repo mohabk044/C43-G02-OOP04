886fe2e369420fdb1c87ccd61eaac517a64f1ab9
Mohab Khaled <mohabk044@gmail.com>
2024-12-27 09:38:56 +0200

    Q 1

---
+++ Assignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using Assignment.Interface_01;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            IAuthenticationService authService = new BasicAuthenticationService();
+
+            Console.WriteLine("Authentication Test");
+            string username = "admin";
+            string password = "password123";
+            if (authService.AuthenticateUser(username, password))
+            {
+                Console.WriteLine($"User '{username}' authenticated successfully.");
+            }
+            else
+            {
+                Console.WriteLine($"Authentication failed for user '{username}'.");
+            }
+
+            Console.WriteLine("\nAuthorization Test");
+            string role = "Administrator";
+            if (authService.AuthorizeUser(username, role))
+            {
+                Console.WriteLine($"User '{username}' is authorized as '{role}'.");
+            }
+            else
+            {
+                Console.WriteLine($"User '{username}' is not authorized as '{role}'.");
+            }
+            #endregion
+
+        }
+    }
+}
